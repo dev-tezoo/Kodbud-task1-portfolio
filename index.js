73e5c4ced9d4a021a728d284d0105a01759fd744
Tejpal Singh <dev.tezoo@gmail.com>
2025-08-30 20:49:10 +0530

    final changes

--- index.js
+++ index.js
@@ -17,3 +17,34 @@ arrowUp.addEventListener('click', () => {
     linksIcon.style.display = 'block';
     arrowUp.style.display = 'none';
 });
+
+
+
+day = document.querySelector('.day')
+night = document.querySelector('.night')
+body = document.querySelector('body')
+page1Text = document.querySelector('.p1')
+page1Text2 = document.querySelector('.p2')
+
+day.addEventListener('click', () => {
+    body.style.backgroundColor = '#ffffff'
+    body.style.color = '#000000'
+    day.style.display = 'none'
+    night.style.display = 'block'
+    page1Text.style.color = '#414141'
+    page1Text2.style.color = '#414141'
+})
+
+night.addEventListener('click', () => {
+    body.style.backgroundColor = '#000000'
+    body.style.color = '#ffffff'
+    night.style.display = 'none'
+    day.style.display = 'block'
+})
+
+
+
+submitBtn = document.querySelector('.form button')
+submitBtn.addEventListener('click', (e) => {
+    alert('Form submitted');
+});
